32920e1bec0d3679844f05e79b392a0d1fe7ac03
Nham Le <nle@lilla-my.cs.uwaterloo.ca>
2018-12-14 23:26:48 -0500

    add /tmp/ to driver

--- driver.py
+++ driver.py
@@ -5,17 +5,17 @@ import sys
 import time
 
 use_cfi = sys.argv[1]
-coutput = sys.argv[2]+"_"+use_cfi+".c"
+coutput = "/tmp/"+sys.argv[2]+"_"+use_cfi+".c"
 
 subprocess.call(("touch %s"%(coutput)).split(" "))
 subprocess.call(("python rop_log_to_c.py --roplog %s --coutput %s --use_cfi %s --config config.json"%("rop/roplog", coutput, use_cfi)).split(" "))
 print(">>>>Start solving")
 start = time.time()
 print(time.ctime())
-subprocess.call(("sea bpf --bound=10 %s -m64 --cex=h.ll --show-invars --inline"%(coutput)).split(" "))
+subprocess.call(("sea bpf --bound=10 %s -m64 --cex=/tmp/h.ll --show-invars --inline"%(coutput)).split(" "))
 end = time.time()
 print(">>>>Finish solving")
 print(time.ctime())
-subprocess.call(("sea exe -m64 %s h.ll -o OUTPUT"%(coutput)).split(" "))
+subprocess.call(("sea exe -m64 %s /tmp/h.ll -o OUTPUT"%(coutput)).split(" "))
 subprocess.call("./OUTPUT")
 print(end - start)
